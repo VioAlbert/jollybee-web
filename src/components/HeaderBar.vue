2dc1ac05f65d66e6a11eec161277cdf817c9e13e
VioAlbert <36184798+VioAlbert@users.noreply.github.com>
2023-12-13 16:59:03 +0700

    Feature: homepage box (#1)
    * Add navigation bar and placeholder contents
    
    * Add footer bar
    
    * Make box size fixed
    
    * Add active class for header

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  defineProps<{
+    pages: Array<String>,
+    activePageIndex: Number
+  }>()
+</script>
+
+<template>
+  <div class="flex">
+    <button v-for="(page, index) in pages"
+      class="border border-white flex-grow text-center text-xl p-1"
+      :class="[
+        activePageIndex === index ? ['bg-white', 'text-[#434343]'] : ''
+      ]"
+      @click="$emit('changePage', index)">
+      {{page}}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
