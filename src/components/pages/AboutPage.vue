3379984523dc70f33ac38371bd6052546d144b44
VioAlbert <36184798+VioAlbert@users.noreply.github.com>
2024-07-12 20:56:01 +0700

    Feature: Add content to pages (#8)
    * Add styling for content
    
    * Add scrollable box
    
    * Add About Page content
    
    * Add more content to About Page
    
    * Add change page functionality to component
    
    * Add content to Learn Page
    
    * Add content to Register Page
    
    * Add more links in About Page
    
    * Fix ul styling

--- src/components/pages/AboutPage.vue
+++ src/components/pages/AboutPage.vue
@@ -1,5 +1,68 @@
 <template>
-  <p>
-    Competitive Programming, or CP in short, is a mind sport in which contestants are given a set of programming problems to be solved within a preset amount of time. Competitions are supported by various companies such as Huawei, Google, Facebook, Shopee, Agoda, Acer, Telegram, Traveloka, Sirclo, and others.
-  </p>
+  <section class="pb-10">
+    <h1>CP</h1>
+    <p>
+    Competitive Programming, or CP in short, is a mind sport in which contestants are given a set of programming problems to be solved within a preset amount of time. Competitions are supported by various companies such as <a href="https://www.huawei.com/minisite/icpc2022/en/">Huawei</a>, <a href="https://codingcompetitionsonair.withgoogle.com/">Google</a>, <a href="https://www.facebook.com/codingcompetitions/hacker-cup">Facebook</a>, <a href="https://careers.shopee.sg/codeleague/">Shopee</a>, <a href="https://codegoda.io/">Agoda</a>, <a href="https://news.acer.com/acer-sponsors-the-international-olympiad-in-informatics-2022">Acer</a>, <a href="https://codeforces.com/blog/entry/13417">Telegram</a>, <a href="https://competition.binus.ac.id/icpc2018/">Traveloka</a>, <a href="http://ngoding-seru.jonathanirvin.gs/">Sirclo</a>, and others.
+    </p>
+  </section>
+
+  <section class="pb-10">
+    <h1>Jollybee</h1>
+    <p>
+    Jollybee is Bina Nusantara University's official CP team. Currently, Jollybee has 30+ active members being coached directlyby the former chief judge of the Indonesian National Science Olympiad in Informatics (OSN bidang Komputer) 2020 and former judge of the International Olympiad in Informatics (IOI) 2022.
+    </p>
+    <br>
+    <p>
+    Jollybee has won numerous national and international competitions, and has represented Indonesia in the ICPC World Finals serveral times.
+    </p>
+    <br>
+    <p>
+    Please visit our <a href="http://instagram.com/binusjollybee">Instagram</a> or <a href="https://www.linkedin.com/company/jollybee-binus/">Linkedin</a> to view our latest achievements.
+    </p>
+  </section>
+
+  <section class="pb-10">
+    <h1>Benefits</h1>
+    <h2>... of learning CP:</h2>
+    <ul>
+      <li>Be proficient at algorithms and data structures</li>
+      <li>Improve your problem solving skills</li>
+      <li>Improve your general coding skills (implementation, debugging, etc.)</li>
+      <li>Learn to think effectively and efficiently</li>
+      <li>Learn to code and solve problems as part of a team</li>
+      <li>Get industry recognition</li>
+      <li>Win achievements for your CV</li>
+    </ul>
+    <br>
+    <h2>... of joining Jollybee:</h2>
+    <ul>
+      <li>Take part in regular CP Training sessions</li>
+      <li>Be part of a solid community with a shared interest in programming</li>
+      <li>Be a judge of BNPCHS, a national CP competition for high school students</li>
+      <li>Help prepare Pelatnas TOKI for Indonesian IOI (International Olympiad in Informatics) representatives</li>
+      <li>Get exclusive apparel</li>
+      <li>Join alumni sharing sessions</li>
+      <li>Visit tech companies</li>
+      <li>All of this for free! (all competition-related expenses are paid by Binus University)</li>
+    </ul>
+  </section>
+
+  <section class="pb-10">
+    <h1>Our Alumni</h1>
+    <p>
+    Competitive Programming has shaped the mindset of our members, refining them into the best version of themselves. Due to their own efforts, some of the companies of our recent alumni are Shopee, Google, Amazon, Meta, Grab, Tiket.com, Traveloka, Tokopedia, Sirclo, and others.
+    </p>
+    <br>
+    <p>
+    Some of our alumni also choose to thrive in education and research as a coach, a lecturer, or a researcher in educational institutes such as the National University of Singapore, University of Tsukuba, and Binus University.
+    </p>
+  </section>
+
+  <section class="pb-10">
+    <center class="space-y-5">
+      <p style="color:orange">&#x25BC;&#x25BC;&#x25BC;</p>
+      <p>What are you waiting for?</p>
+      <button class="register-btn" @click="$emit('changePage', 2)">Register</button>
+    </center>
+  </section>
 </template>
